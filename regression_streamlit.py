a0a271673b5b58cdba15cefc4977bcd382f48f5d
Lilia <44172839+leskivl@users.noreply.github.com>
2020-05-06 23:20:08 +0300

    Add files via upload

---
+++ regression_streamlit.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+rcParams['figure.figsize'] = 14, 8
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+
+df_train = pd.read_csv('train.csv')
+
+sns.distplot(df_train['SalePrice']);
+st.pyplot()
+
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000), s=32);
+st.pyplot()
+
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
+st.pyplot()
+
+
+x = df_train['GrLivArea']
+y = df_train['SalePrice']
+
+x = (x - x.mean()) / x.std()
+x = np.c_[np.ones(x.shape[0]), x] 
+
+@st.cache
+def loss(h, y):
+  sq_error = (h - y)**2
+  n = len(y)
+  return 1.0 / (2*n) * sq_error.sum()
+
+@st.cache
+class LinearRegression:
+    
+  def predict(self, X):
+    return np.dot(X, self._W)
+  def _gradient_descent_step(self, X, targets, lr):
+
+    predictions = self.predict(X)
+    
+    error = predictions - targets
+    gradient = np.dot(X.T,  error) / len(X)
+
+    self._W -= lr * gradient
+    
+  def fit(self, X, y, n_iter=100000, lr=0.01):
+
+    self._W = np.zeros(X.shape[1])
+
+    self._cost_history = []
+    self._w_history = [self._W]
+    for i in range(n_iter):
+      
+        prediction = self.predict(X)
+        cost = loss(prediction, y)
+        
+        self._cost_history.append(cost)
+        
+        self._gradient_descent_step(x, y, lr)
+        
+        self._w_history.append(self._W.copy())
+    return self
+
+clf = LinearRegression()
+clf.fit(x, y, n_iter=2000, lr=0.01)
+
+yyy = clf.predict(x)
+
+
+plt.title('Cost Function J')
+plt.xlabel('No. of iterations')
+plt.ylabel('Cost')
+plt.plot(clf._cost_history)
+st.pyplot()
+
+
+fig = plt.figure()
+ax = plt.axes()
+plt.title('Sale Price vs Living Area')
+plt.xlabel('Living Area in square feet (normalised)')
+plt.ylabel('Sale Price ($)')
+plt.scatter(x[:,1], y)
+plt.plot(x, yyy, color='red')
+st.pyplot()
